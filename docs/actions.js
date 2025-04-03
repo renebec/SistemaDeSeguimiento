51c4e83b3ededb783c67bf87aadb4f76d7f5cc83
renebec <39104197+renebec@users.noreply.github.com>
2025-04-03 13:48:39 -0600

    Add files via upload

---
+++ docs/actions.js
@@ -0,0 +1,32 @@
+function PopWinClose()
+{
+    window.close();
+}
+
+function PopWinHome()
+{
+    window.location.replace("index.html","width=100%", "_self");
+}
+
+
+function PopWin_2TAGP()
+{
+    window.location.replace("PopWin_2TAGP.html","width=100%", "_self");
+}
+
+function PopWin_6TAGP()
+{
+    window.location.replace("PopWin_6TAGP.html","width=100%", "_self");
+}
+
+
+function PopWin_6TSMEC()
+{
+    window.location.replace("PopWin_6TSMEC.html","width=100%", "_self");
+}
+
+
+function PopWin_6TAEA()
+{
+    window.location.replace("PopWin_6TAEA.html","width=100%", "_self");
+}
